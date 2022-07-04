8f1139a50a00dc12a3861d76909327e8bb8292e0
Fikri Fadillah <fkrfdllh@gmail.com>
2022-07-04 10:53:47 +0700

    Add Environment and Database setup inside config package

---
+++ src/config/Database.java
@@ -0,0 +1,45 @@
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author fkrfd
+ */
+public class Database {
+
+    private static Connection connection = null;
+
+    public static Connection getConnection() {
+        if (connection == null) {
+
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+
+                connection = DriverManager.getConnection("jdbc:mysql://" + Environment.HOST + "/" + Environment.DB_NAME, Environment.DB_USER, Environment.DB_PASSWORD);
+            } catch (Exception e) {
+                e.printStackTrace();
+
+                return null;
+            }
+
+        }
+
+        return connection;
+    }
+
+    public static boolean isConnect() {
+        try {
+            if (getConnection() != null) {
+                return true;
+            }
+        } catch (Exception e) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, e);
+        }
+
+        return false;
+    }
+}
---
+++ src/config/Environment.java
@@ -0,0 +1,14 @@
+package config;
+
+/**
+ *
+ * @author fkrfd
+ */
+public class Environment {
+
+    public static String HOST = "localhost";
+
+    public static String DB_USER = "root";
+    public static String DB_PASSWORD = "";
+    public static String DB_NAME = "ticket";
+}
